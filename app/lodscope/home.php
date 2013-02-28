ea4058d509412384828f28f3d562493a167d34b9
Mo McRoberts <mo@nevali.net>
2013-02-28 20:54:45 +0000

    Handle RDFa parsing, add some more predefined namespaces, derive alternative serialisation links from the RDFDocument instance itself, instead of hardcoding; add some more examples.

--- app/lodscope/home.php
+++ app/lodscope/home.php
@@ -24,41 +24,50 @@ class LODScopeHomepage extends Page
 	protected $templateName = 'home.phtml';
 	protected $uri = null;
 
-	protected $supportedTypes = array(
-		'text/html',
-		'text/turtle',
-		'application/rdf+xml',
-		'application/n3',
-		'application/n-triples',
-		'text/n3' => array('hide' => true),
-		'text/plain' => array('hide' => true),
-		'text/rdf+n3' => array('hide' => true),
-		'application/ld+json',
-		'application/json',
-		);
-	
-		public function __construct()
-		{
-			parent::__construct();
-			RDF::registerPrefix('og', 'http://ogp.me/ns#');
-			RDF::registerPrefix('fb', 'http://ogp.me/ns/fb#');
-			RDF::registerPrefix('user', 'http://graph.facebook.com/schema/user#');
-			RDF::registerPrefix('schema', 'http://schema.org/');
-			RDF::registerPrefix('wo', 'http://purl.org/ontology/wo/');
-			RDF::registerPrefix('po', 'http://purl.org/ontology/po/');
-			RDF::registerPrefix('cc', 'http://creativecommons.org/ns#');
-			RDF::registerPrefix('skos2004', 'http://www.w3.org/2004/02/skos/core#');
-		}
+	public function __construct()
+	{
+		parent::__construct();
+		RDF::registerPrefix('og', 'http://ogp.me/ns#');
+		RDF::registerPrefix('fb', 'http://ogp.me/ns/fb#');
+		RDF::registerPrefix('user', 'http://graph.facebook.com/schema/user#');
+		RDF::registerPrefix('schema', 'http://schema.org/');
+		RDF::registerPrefix('wo', 'http://purl.org/ontology/wo/');
+		RDF::registerPrefix('po', 'http://purl.org/ontology/po/');
+		RDF::registerPrefix('cc', 'http://creativecommons.org/ns#');
+		RDF::registerPrefix('skos2004', 'http://www.w3.org/2004/02/skos/core#');
+		RDF::registerPrefix('bibo', 'http://purl.org/ontology/bibo/');
+		RDF::registerPrefix('doi', 'http://dx.doi.org/');
+		RDF::registerPrefix('nature', 'http://ns.nature.com/terms/');
+		RDF::registerPrefix('prism', 'http://prismstandard.org/namespaces/basic/2.1/');
+	}
 	
 	protected function getObject()
 	{
-		if(isset($this->request->query['uri']))
+		if($this->object === null)
 		{
-			$this->uri = trim($this->request->query['uri']);
+			if(isset($this->request->query['uri']))
+			{
+				$this->uri = trim($this->request->query['uri']);
+			}
+			if(strlen($this->uri))
+			{
+				$this->object = RDF::documentFromURL($this->uri);
+			}
 		}
-		if(strlen($this->uri))
+		if($this->object !== null)
 		{
-			$this->object = RDF::documentFromURL($this->uri);
+			$serialisations = $this->object->serialisations();
+			foreach($serialisations as $key => $info)
+			{
+				if(is_numeric($key))
+				{
+					$this->supportedTypes[] = $info;
+				}
+				else
+				{
+					$this->supportedTypes[$key] = $info;
+				}
+			}
 		}
 		return true;
 	}
@@ -80,7 +89,52 @@ class LODScopeHomepage extends Page
 	protected function assignTemplate()
 	{
 		parent::assignTemplate();
+		$serialisations = array();
+		unset($this->supportedTypes['application/x-xmp+xml']['hide']);
+		foreach($this->supportedTypes as $k => $info)
+		{
+			if(!is_array($info))
+			{
+				$info = array('type' => $info);
+			}
+			$resource = $this->request->resource;
+			if(isset($this->uri))
+			{
+				$query = '?uri=' . urlencode($this->uri);
+			}
+			else
+			{
+				$query = '';
+			}
+			if(!isset($info['type'])) $info['type'] = $k;
+			if(!empty($info['hide'])) continue;
+			if($info['type'] == 'text/html') continue;
+			$location = $resource . MIME::extForType($info['type']) . $query;
+			$link = array('rel' => 'alternate', 'type' => $info['type'], 'href' => $location);
+			if(isset($info['title']))
+			{
+				$link['title'] = $info['title'];
+				$serialisations[$location] = $info;
+				$serialisations[$location]['alt'] = array();
+			}
+			$this->links[] = $link;
+			if(isset($info['alt']))
+			{
+				foreach($info['alt'] as $mime => $title)
+				{
+					$altloc = $resource . MIME::extForType($mime) . $query;
+					if(isset($info['title']))
+					{
+						$serialisations[$location]['alt'][$altloc] = array('title' => $title);
+						$title = $info['title'] . ' (' . $title . ')';
+					}
+					$link = array('rel' => 'alternate', 'type' => $mime, 'title' => $title, 'href' => $altloc);
+					$this->links[] = $link;
+				}
+			}
+		}
 		$this->vars['uri'] = $this->uri;
+		$this->vars['serialisations'] = $serialisations;
 		if(isset($this->object))
 		{
 			$this->object->linkFilter = array($this, 'linkFilter');
